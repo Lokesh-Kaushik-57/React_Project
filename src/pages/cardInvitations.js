88e6c5cf177e75278fc018da198ca7ca863f68b4
Lokesh Kaushik <lokeshkaushik57@gmail.com>
2023-07-06 19:24:19 +0530

    Update Project

--- src/pages/cardInvitations.js
+++ src/pages/cardInvitations.js
@@ -1,43 +1,99 @@
-import React from 'react'
-import Layout from '../Components/Layout/Layout'
-import { Link } from 'react-router-dom';
-import Carda from '../Images/card1.webp'
-import Cardb from '../Images/card2.webp'
-import Cardc from '../Images/card3.webp'
-import Cardd from '../Images/card4.webp'
-import '../styles/CardInvitations.css'
+import React from "react";
+import Layout from "../Components/Layout/Layout";
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../styles/CardInvitations.css"
+import Card1 from '../Images/card1.webp'
+import Card2 from '../Images/card2.webp'
+import Card3 from '../Images/card3.webp'
+import Card4 from '../Images/card4.webp'
+import Card5 from '../Images/card5.webp'
+import Card6 from '../Images/card6.webp'
+import { Link } from "react-router-dom";
 
-const Cards = () => {
+
+const CustomPrevArrow = (props) => (
+  <div
+    className="custom-prev-arrow"
+    onClick={props.onClick}
+    style={{ color: '#FF1493' }} // Change the color here
+  >
+    <ArrowCircleLeftIcon />
+  </div>
+);
+
+const CustomNextArrow = (props) => (
+  <div
+    className="custom-next-arrow"
+    onClick={props.onClick}
+    style={{ color: '#FF1493'  }} // Change the color here
+  >
+    <ArrowCircleRightIcon />
+  </div>
+);
+
+const Carousel = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <Layout>
-      <div className='invite-page'>
-        <h1>Wedding Cards</h1>
-        <div className='cards'>
-          <div>
-            <img className='card1 invitation' src={Carda} alt='not found' />
-            <h3>Charmed Theme</h3>
-          </div>
-          <div>
-            <img className='card2 invitation' src={Cardb} alt='not found' />
-            <h3>Bohemian Theme</h3>
-          </div>
-          <div>
-            <img className='card3 invitation' src={Cardc} alt='not found' />
-            <h3>Tulip Garden Theme</h3>
-          </div>
-          <div>
-            <img className='card4 invitation' src={Cardd} alt='not found' />
-            <h3>Vivacious Pastel Theme</h3>
-          </div>
-        </div>
-          <Link to='/card-invitations/sample-card-slide'>
-            <button className='button'>
-              View Sample Card
-            </button>
-          </Link>
-        </div>
-    </Layout>
-  )
-}
 
-export default Cards
+    <div className="carousel-container">
+      <Slider {...settings}>
+        <div className="carousel-item">
+          <img src={Card1} alt="not found" />
+        </div>
+        <div className="carousel-item">
+          <img src={Card2} alt="not found" />
+        </div>
+        <div className="carousel-item">
+          <img src={Card3} alt="not found" />
+        </div>
+        <div className="carousel-item">
+          <img src={Card4} alt="not found" />
+        </div>
+        <div className="carousel-item">
+          <img src={Card5} alt="not found" />
+        </div>
+        <div className="carousel-item">
+          <img src={Card6} alt="not found" />
+        </div>
+      </Slider>
+    </div>
+    <Link to='/card-invitations/sample-card-slide'>
+             <button className='button'>
+              View Sample Card
+             </button>
+          </Link>
+    </Layout>
+  );
+};
+
+export default Carousel;
